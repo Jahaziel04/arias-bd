cb66bd4a82e454589070d17a950e234bd9d46eab
Jahaziel Ortiz Barrios <jahaziel041090@hotmail.com>
2016-02-12 16:18:02 -0600

    Retoques

--- js/mains.js
+++ js/mains.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
       tick,
       percentTime;
 
+
 $(".owl-carousel").owlCarousel({
  
 
@@ -57,7 +58,7 @@ $(".owl-carousel").owlCarousel({
     autoHeight : false,
  
     //JSON 
-    jsonPath : "./js/headers.json", 
+    jsonPath : "./js/json/headers.json", 
     jsonSuccess : false,
  
     //Mouse Events
@@ -82,6 +83,232 @@ $(".owl-carousel").owlCarousel({
     startDragging : pauseOnDragging,
     afterLazyLoad : false
   });
+
+
+$(".owl-carousel-entrada").owlCarousel({
+ 
+
+    items : 1,
+    itemsDesktop : [1199,1],
+    itemsDesktopSmall : [979,1],
+    itemsTablet: [768,1],
+    itemsTabletSmall: false,
+    itemsMobile : [479,1],
+    singleItem : true,
+    itemsScaleUp : false,
+ 
+    //Basic Speeds
+    slideSpeed : 700,
+    paginationSpeed : 2000,
+    rewindSpeed : 1000,
+ 
+    //Autoplay
+    autoPlay : true,
+    stopOnHover : true,
+ 
+    // Navigation
+    navigation : false,
+    navigationText : ["previo","siguiente"],
+    rewindNav : true,
+    scrollPerPage : false,
+ 
+    //Pagination
+    pagination : true,
+    paginationNumbers: false,
+ 
+    // Responsive 
+    responsive: true,
+    responsiveRefreshRate : 200,
+    responsiveBaseWidth: window,
+ 
+    // CSS Styles
+    baseClass : "owl-carousel",
+    theme : "owl-theme",
+ 
+    //Lazy load
+    lazyLoad : false,
+    lazyFollow : true,
+    lazyEffect : "fade",
+ 
+    //Auto height
+    autoHeight : false,
+ 
+    //JSON 
+    jsonPath : "./js/json/entrada.json", 
+    jsonSuccess : false,
+ 
+    //Mouse Events
+    dragBeforeAnimFinish : true,
+    mouseDrag : true,
+    touchDrag : true,
+ 
+    //Transitions
+    transitionStyle : false,
+ 
+    // Other
+    addClassActive : false,
+ 
+    //Callbacks
+    beforeUpdate : false,
+    afterUpdate : false,
+    beforeInit: false, 
+    afterInit: progressBar, 
+    beforeMove: false, 
+    afterMove: moved,
+    afterAction: false,
+    startDragging : pauseOnDragging,
+    afterLazyLoad : false
+  });
+$(".owl-carousel-honor").owlCarousel({
+ 
+
+    items : 1,
+    itemsDesktop : [1199,1],
+    itemsDesktopSmall : [979,1],
+    itemsTablet: [768,1],
+    itemsTabletSmall: false,
+    itemsMobile : [479,1],
+    singleItem : true,
+    itemsScaleUp : false,
+ 
+    //Basic Speeds
+    slideSpeed : 700,
+    paginationSpeed : 2000,
+    rewindSpeed : 1000,
+ 
+    //Autoplay
+    autoPlay : true,
+    stopOnHover : true,
+ 
+    // Navigation
+    navigation : false,
+    navigationText : ["previo","siguiente"],
+    rewindNav : true,
+    scrollPerPage : false,
+ 
+    //Pagination
+    pagination : true,
+    paginationNumbers: false,
+ 
+    // Responsive 
+    responsive: true,
+    responsiveRefreshRate : 200,
+    responsiveBaseWidth: window,
+ 
+    // CSS Styles
+    baseClass : "owl-carousel",
+    theme : "owl-theme",
+ 
+    //Lazy load
+    lazyLoad : false,
+    lazyFollow : true,
+    lazyEffect : "fade",
+ 
+    //Auto height
+    autoHeight : false,
+ 
+    //JSON 
+    jsonPath : "./js/json/honor.json", 
+    jsonSuccess : false,
+ 
+    //Mouse Events
+    dragBeforeAnimFinish : true,
+    mouseDrag : true,
+    touchDrag : true,
+ 
+    //Transitions
+    transitionStyle : false,
+ 
+    // Other
+    addClassActive : false,
+ 
+    //Callbacks
+    beforeUpdate : false,
+    afterUpdate : false,
+    beforeInit: false, 
+    afterInit: progressBar, 
+    beforeMove: false, 
+    afterMove: moved,
+    afterAction: false,
+    startDragging : pauseOnDragging,
+    afterLazyLoad : false
+  });
+
+$(".owl-carousel-jardineras").owlCarousel({
+ 
+
+    items : 1,
+    itemsDesktop : [1199,1],
+    itemsDesktopSmall : [979,1],
+    itemsTablet: [768,1],
+    itemsTabletSmall: false,
+    itemsMobile : [479,1],
+    singleItem : true,
+    itemsScaleUp : false,
+ 
+    //Basic Speeds
+    slideSpeed : 700,
+    paginationSpeed : 2000,
+    rewindSpeed : 1000,
+ 
+    //Autoplay
+    autoPlay : true,
+    stopOnHover : true,
+ 
+    // Navigation
+    navigation : false,
+    navigationText : ["previo","siguiente"],
+    rewindNav : true,
+    scrollPerPage : false,
+ 
+    //Pagination
+    pagination : true,
+    paginationNumbers: false,
+ 
+    // Responsive 
+    responsive: true,
+    responsiveRefreshRate : 200,
+    responsiveBaseWidth: window,
+ 
+    // CSS Styles
+    baseClass : "owl-carousel",
+    theme : "owl-theme",
+ 
+    //Lazy load
+    lazyLoad : false,
+    lazyFollow : true,
+    lazyEffect : "fade",
+ 
+    //Auto height
+    autoHeight : false,
+ 
+    //JSON 
+    jsonPath : "./js/json/jardineras.json", 
+    jsonSuccess : false,
+ 
+    //Mouse Events
+    dragBeforeAnimFinish : true,
+    mouseDrag : true,
+    touchDrag : true,
+ 
+    //Transitions
+    transitionStyle : false,
+ 
+    // Other
+    addClassActive : false,
+ 
+    //Callbacks
+    beforeUpdate : false,
+    afterUpdate : false,
+    beforeInit: false, 
+    afterInit: progressBar, 
+    beforeMove: false, 
+    afterMove: moved,
+    afterAction: false,
+    startDragging : pauseOnDragging,
+    afterLazyLoad : false
+  });
+
  
  //Init progressBar where elem is $("#owl-demo")
     function progressBar(elem){
