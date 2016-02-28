883e8446cc5f70c923a8ad10984e0f2d0f55184b
Jahaziel Ortiz Barrios <jahaziel041090@hotmail.com>
2016-02-28 14:19:45 -0600

    Quitan codigo extra

--- js/mains.js
+++ js/mains.js
@@ -1,3 +1,8 @@
+window.onload = function() {
+    // Intensify all images with the 'intense' classname.
+    var elements = document.querySelectorAll( '.intense' );
+    Intense( elements );
+}
 $(document).ready(function() {
 
 $(".owl-carousel").owlCarousel({
